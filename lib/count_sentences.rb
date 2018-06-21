7a4329f7bcd8c6e6ca32e420423e1131f2493425
Valerie Moore <periwinkle17@outlook.com>
2018-06-21 18:00:12 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,11 +10,17 @@ class String
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+  if self.end_with?("!")
+      true
+    else false
+    end
   end
 
   def count_sentences
