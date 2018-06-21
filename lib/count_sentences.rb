a7ac97a1a8d2f0b9f2b97a9af68c9d788ddb3041
Valerie Moore <periwinkle17@outlook.com>
2018-06-21 18:10:29 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -24,9 +24,7 @@ class String
   end
 
   def count_sentences
-    new_array << self.split("." || "?" || "!")
+    self.split("." || "?" || "!")
     binding.pry
-    new_array.join(" ")
-    new_array.count
   end
 end
